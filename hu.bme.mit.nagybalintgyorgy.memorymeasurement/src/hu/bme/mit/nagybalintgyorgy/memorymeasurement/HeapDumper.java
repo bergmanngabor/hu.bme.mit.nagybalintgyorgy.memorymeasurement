3a48024fbdd9263cbb9adc156bd4202b57ddccde
nagybalint1993 <nagybalint1993@gmail.com>
2016-10-24 18:23:21 +0200

    first commit
    ++class HeapDumper
    
    
    
    
    Signed-off-by: nagybalint1993 <nagybalint1993@gmail.com>

---
+++ hu.bme.mit.nagybalintgyorgy.memorymeasurement/src/hu/bme/mit/nagybalintgyorgy/memorymeasurement/HeapDumper.java
@@ -0,0 +1,60 @@
+package hu.bme.mit.nagybalintgyorgy.memorymeasurement;
+import javax.management.MBeanServer;
+import java.io.IOException;
+import java.lang.management.ManagementFactory;
+import com.sun.management.HotSpotDiagnosticMXBean;
+
+public class HeapDumper {
+	// This is the name of the HotSpot Diagnostic MBean
+    private static final String HOTSPOT_BEAN_NAME =
+         "com.sun.management:type=HotSpotDiagnostic";
+	
+	
+	// field to store the hotspot diagnostic MBean 
+    private static volatile HotSpotDiagnosticMXBean hotspotMBean;
+	
+	public HeapDumper(){
+		initHotspotMBean();
+	}
+	
+	public boolean createHeapDump(String filename){
+		
+		try {
+			// second arg is true because I need only live objects
+			hotspotMBean.dumpHeap(filename, true);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+		
+		return true;
+	}
+	
+	// initialize the hotspot diagnostic MBean field
+    private static void initHotspotMBean() {
+        if (hotspotMBean == null) {
+            synchronized (HeapDumper.class) {
+                if (hotspotMBean == null) {
+                    hotspotMBean = getHotspotMBean();
+                }
+            }
+        }
+    }
+    
+    // get the hotspot diagnostic MBean from the
+    // platform MBean server
+    private static HotSpotDiagnosticMXBean getHotspotMBean() {
+        try {
+            MBeanServer server = ManagementFactory.getPlatformMBeanServer();
+            HotSpotDiagnosticMXBean bean = 
+                ManagementFactory.newPlatformMXBeanProxy(server,
+                HOTSPOT_BEAN_NAME, HotSpotDiagnosticMXBean.class);
+            return bean;
+        } catch (RuntimeException re) {
+            throw re;
+        } catch (Exception exp) {
+            throw new RuntimeException(exp);
+        }
+    }
+}
