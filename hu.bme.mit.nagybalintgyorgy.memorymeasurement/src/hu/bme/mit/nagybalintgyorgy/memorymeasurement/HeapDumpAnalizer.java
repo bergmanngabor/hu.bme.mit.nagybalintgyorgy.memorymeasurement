cb39209f51a1e22a0fa9dcbb138fb1b02b5f6971
nagybalint1993 <nagybalint1993@gmail.com>
2016-11-06 16:37:51 +0100

    ++

--- hu.bme.mit.nagybalintgyorgy.memorymeasurement/src/hu/bme/mit/nagybalintgyorgy/memorymeasurement/HeapDumpAnalizer.java
+++ hu.bme.mit.nagybalintgyorgy.memorymeasurement/src/hu/bme/mit/nagybalintgyorgy/memorymeasurement/HeapDumpAnalizer.java
@@ -7,12 +7,15 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
+import org.eclipse.emf.ecore.xml.type.util.XMLTypeResourceImpl.StackFrame;
 import org.eclipse.mat.SnapshotException;
 import org.eclipse.mat.snapshot.ISnapshot;
 import org.eclipse.mat.snapshot.SnapshotFactory;
 import org.eclipse.mat.snapshot.SnapshotInfo;
 import org.eclipse.mat.snapshot.model.IClass;
 import org.eclipse.mat.snapshot.model.IObject;
+import org.eclipse.mat.snapshot.model.IStackFrame;
+import org.eclipse.mat.snapshot.model.IThreadStack;
 import org.eclipse.mat.util.VoidProgressListener;
 
 public class HeapDumpAnalizer {
@@ -27,10 +30,27 @@ public class HeapDumpAnalizer {
 
 		try {
 			ISnapshot snapshot = SnapshotFactory.openSnapshot(new File(dumpname), new VoidProgressListener());
+			long size=0;
+			
+			IThreadStack stack= snapshot.getThreadStack((int) Thread.currentThread().getId());
+			IStackFrame[] stackframes= stack.getStackFrames();
+			
+			for(int i=0; i< stackframes.length ; i++){
+				int[] ids= stackframes[i].getLocalObjectsIds();
+				for (int j=0; j<ids.length;i++ ){
+					int id= ids[i];
+					size += snapshot.getRetainedHeapSize(id);
+					System.out.println(id + " *****   " + size);
+					
+				}
+			}
+			
+			
+			/*
 			List<IClass> classes= (List<IClass>) snapshot.getClasses();
 			VoidProgressListener vp= new VoidProgressListener();
 			
-			long size=0;
+			
 			
 	
 			Set<Integer> set= new HashSet<Integer>();
@@ -54,6 +74,10 @@ public class HeapDumpAnalizer {
 			
 			
 			System.out.println("vagy: száma:" + idz.length  +  "  \n Mérete:" + size2);
+			
+			
+			*/
+			
 			return size;
 			
 			
